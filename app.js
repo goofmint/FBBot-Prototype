b1493aec91d09c26b9a22f21ae82c2a6929031af
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-04-12 15:16:22 -0700

    Init

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === process.env.FBBOT_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, text);
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
+var token = "<page_access_token>";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
