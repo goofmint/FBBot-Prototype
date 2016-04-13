f971de1d104a85936778c1311ce18fdf8cddc957
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-04-12 17:07:49 -0700

    Support template message

--- app.js
+++ app.js
@@ -21,14 +21,66 @@ app.post('/webhook/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      sendTextMessage(sender, text);
+      if (text === 'template') {
+        sendTemplateMessage(sender);
+      } else {
+        sendTextMessage(sender, text);
+      }
       // Handle a text message from this sender
     }
   }
   res.sendStatus(200);
 });
 
-var token = "<page_access_token>";
+function sendTemplateMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  var token = process.env.FBPAGE_TOKEN;
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });  
+}
 
 function sendTextMessage(sender, text) {
   messageData = {
